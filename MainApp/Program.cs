565956989d975f4b9406191bc67128b5dfa5c98b
Alican Mavi <mavia240@gmail.com>
2024-11-25 13:40:56 +0100

    First/Last Name added

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hejsan");
+﻿Console.WriteLine("Hej och välkommen");
+Console.WriteLine("Var vänligen och fyll i informationen här nedan.");
+
+Console.Write("Förnamn: ");
+    string firstName = Console.ReadLine()!;
+
+Console.Write("Efternamn: ");
+string lastName = Console.ReadLine()!;
